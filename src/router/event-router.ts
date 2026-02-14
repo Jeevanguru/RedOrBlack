0e94ac3b65618250d70f120567ee8cb596d4aa0c
Jeevan S <130644785+Jeevanguru@users.noreply.github.com>
2026-02-14 12:07:21 +0530

    Delete src directory

--- src/router/event-router.ts
+++
@@ -1,7 +0,0 @@
-import { Server } from "socket.io";
-import { initRounds } from "../module/lobbies/lobby-event.js";
-
-export const eventRouter = async (io: Server): Promise<void> => {
-    console.log("Initializing rounds");
-    initRounds(io);
-};
